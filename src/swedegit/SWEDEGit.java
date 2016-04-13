16b30a0e44b9a09a8df1d377741ff29abbad1d60
Toninatonina@miko <Toninatonina@miko>
2016-04-13 19:33:43 +0200

    Primer canvi

--- src/swedegit/SWEDEGit.java
+++ src/swedegit/SWEDEGit.java
@@ -15,7 +15,7 @@ public class SWEDEGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Hola món!");
     }
     
 }
